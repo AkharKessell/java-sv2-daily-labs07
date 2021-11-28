383d2763e1710d173b908ceac86c270ab8d72540
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-28 11:52:05 +0100

    Készíts egy day01.Human nevű osztályt. Attribútumai  legyenek a neve, a születési éve. Konstruktorban állítsd be az  attribútumokat, de figyelj arra, hogy a program futásának pillanatában  senki nem lehet 120 évesnél öregebb, tehát ha ez nem teljesül, akkor dobjunk IllegalArgumentException kivételt! Fontos még, hogy a neve is legalább két részből kell, hogy álljon (vezeték- és keresztnév)! Készíts unit teszteket!

---
+++ Week07/src/main/java/Day01/Human.java
@@ -0,0 +1,37 @@
+package Day01;
+
+import java.time.LocalDate;
+
+public class Human {
+    private String name;
+    private int yearOfBirth;
+
+    public Human(String name, int yearOfBirth) {
+        if (isNameValid(name) && isYearValid(yearOfBirth)) {
+            this.name = name;
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
+
+    private boolean isYearValid(int yearOfBirth) {
+        if (LocalDate.now().getYear() - yearOfBirth > 120) {
+            throw new IllegalArgumentException("Your age can't be higher than 120 years.");
+        }
+        return true;
+    }
+
+    private boolean isNameValid(String name) {
+        if (!(name != null && name.trim().contains(" "))) {
+            throw new IllegalArgumentException("Enter a firstname and a lastname as well.");
+        }
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+}

b13585a03b57516c0146a76e600311c3dfd82e31
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-28 11:53:49 +0100

    A feladatban egy biciklis futár egy heti munkáját rögzítjük és készítünk statisztikákat.
    A futár minden fuvar után feljegyzi, hogy a hét hányadik  napján történt a fuvar. Ezután azt, hogy az adott nap hányadik fuvarját  teljesítette és ezután azt, hogy hány kilométer volt az adott fuvar. A  futár egy-egy fuvarját reprezentálja a Ride nevű osztály, adattagokkal, konstruktorra, getterekkel.
    
    Készítsd el a Courier osztályt, ez fogja a futárt reprezentálni. Legyen egy rides listája, ami fuvarokat tárol. Legyen egy addRide(Ride ride) metódus, ami csak sorrendben enged hozzáadni elemeket a listához. Figyeljünk viszont arra, hogy nem feltétlenül minden nap dolgozott a  futár, de ha már bekerült egy 3. napi fuvar, akkor ne kerülhessen be egy 2. napi. És arra is figyelj, hogy a napon belül is sorrendben  kerüljenek be az adatok. Ha a paraméterül kapott Ride nem felel meg a feltételeknek dobjunk IllegalArgumentException-t.
    
    Legyen egy CourierFileManager, amiben van egy createCourierByFile(Path path) nevű metódus, ami egy paraméterül kapott file alapján elkészít egy futár objektumot és visszaadja azt!
    
    Egy minta file:
    
    1 1 12
    1 2 11
    2 1 10
    4 1 19
    4 2 5
    4 3 9
    
    Itt látható, hogy a futár nem dolgozott a 3. 5. 6. és 7. napon.

---
+++ Week07/src/main/java/Day04/Courier.java
@@ -0,0 +1,30 @@
+package Day04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Courier {
+    private List<Ride> rides = new ArrayList<>();
+
+    public List<Ride> getRides() {
+        return rides;
+    }
+
+    public boolean addRide(Ride ride) {
+        if (rides.size() == 0) {
+            return rides.add(ride);
+        }
+
+        int lastDay = rides.get(rides.size() - 1).getDayOfRide();
+        if (lastDay < ride.getDayOfRide()) {
+            return rides.add(ride);
+        }
+
+        int lastRide = rides.get(rides.size() - 1).getRideOfDay();
+        if (lastRide < ride.getRideOfDay()) {
+            return rides.add(ride);
+        }
+
+        throw new IllegalArgumentException("New ride data is not in sync with previous data.");
+    }
+}
---
+++ Week07/src/main/java/Day04/Ride.java
@@ -0,0 +1,25 @@
+package Day04;
+
+public class Ride {
+    private int dayOfRide;
+    private int rideOfDay;
+    private double km;
+
+    public Ride(int dayOfRide, int rideOfDay, double km) {
+        this.dayOfRide = dayOfRide;
+        this.rideOfDay = rideOfDay;
+        this.km = km;
+    }
+
+    public int getDayOfRide() {
+        return dayOfRide;
+    }
+
+    public int getRideOfDay() {
+        return rideOfDay;
+    }
+
+    public double getKm() {
+        return km;
+    }
+}
